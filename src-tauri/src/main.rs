b3709f6754565cd2790f6c807df5775227cb57ee
arrebole <concurrent.exec@gmail.com>
2023-02-13 18:42:01 +0800

    first init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+
+use std::fs;
+// use std::env;
+
+#[tauri::command]
+fn rules() -> String {
+    // let path = env::current_dir();
+    // println!("The current directory is {}", path.expect("REASON").display());
+    let result = fs::read_to_string("./interparser.json").unwrap();
+    return  result.into();
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![rules])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <h2 class="title">国际短信手机号分析助手</h2>
+  <div class="main">
+    <input class="input" name="file" type="file" @input="handleInput" />
+    <div> <a download="result.csv" :href="state.url">{{ state.lable }}</a></div>
+  </div>
+</template>
+
+
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import { invoke } from '@tauri-apps/api';
+
+const state = reactive({
+  url: '',
+  lable: '下载结果'
+})
+
+function readFile(file: File): Promise<string> {
+  const reader = new FileReader()
+  reader.readAsText(file);
+  return new Promise((resolve) => {
+    reader.onload = () => {
+      resolve(reader.result as string)
+    }
+  })
+}
+
+function showDownload(data: string) {
+  state.url = window.URL.createObjectURL(
+    new Blob([data], { type: "text/plain" }),
+  );
+}
+
+async function handleInput(e: Event) {
+  // 文本内容
+  // @ts-ignore
+  const data = await readFile(e.target.files[0] as File);
+
+  // 手机号池
+  const datasource = new Map<string, number>();
+  const rules = JSON.parse(await invoke('rules') as string);
+
+  data.split('\n').forEach((line) => {
+    const [mobile, count] = line.split(',');
+    if (!mobile || isNaN(parseInt(count))) {
+      return;
+    }
+
+    for (const key in rules) {
+      const regs: RegExp[] = rules[key].map((v: string) => new RegExp(v));
+      if (regs.map(r => r.test(mobile)).includes(true)) {
+        if (datasource.has(key)) {
+          datasource.set(key, datasource.get(key)! + parseInt(count))
+        } else {
+          datasource.set(key, parseInt(count));
+        }
+        return
+      }
+    }
+  });
+
+  console.log("完成")
+  return showDownload(
+    Array.from(datasource.keys()).map(k => `${k}, ${datasource.get(k)}`).join('\n')
+  );
+}
+
+</script>
+
+
+<style>
+.title {
+  text-align: center;
+}
+
+.main {
+  text-align: center;
+  vertical-align: middle;
+}
+
+.input {
+  padding: 2rem;
+  margin: 1.4rem;
+}
+</style>
+
